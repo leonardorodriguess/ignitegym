67b077566ff11ff64abada189f79fab75e926d85
leonardo <leo-feitosa@live.com>
2024-01-14 18:16:17 -0300

    style: statusbar

--- App.tsx
+++ App.tsx
@@ -1,16 +1,21 @@
-import { Text, View } from "react-native";
+import { Text, View, StatusBar } from 'react-native';
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
+} from '@expo-google-fonts/roboto';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
-      {fontsLoaded? <Text>Hello World!</Text>: <View/>}
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <StatusBar
+        translucent
+        barStyle={'light-content'}
+        backgroundColor={'transparent'}
+      />
+      {fontsLoaded ? <Text>Hello World!</Text> : <View />}
     </View>
   );
 }
