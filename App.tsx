b7d217e163f41895f82563021cb9fa17e923a722
Leonardo <leonardorodrigues@grendene.com.br>
2024-02-02 09:27:03 -0300

    config: add gluestack

--- App.tsx
+++ App.tsx
@@ -4,18 +4,23 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
+import { StyledProvider, styled } from '@gluestack-style/react';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
+// import { config } from '@gluestack-ui/config';
+import { config } from './config/gluestack-ui.config';
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
-
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <StyledProvider config={config }>
       <StatusBar
         translucent
         barStyle={'light-content'}
         backgroundColor={'transparent'}
       />
-      {fontsLoaded ? <Text>Hello World!</Text> : <View />}
-    </View>
+      <Loading />
+    </StyledProvider>
   );
 }
