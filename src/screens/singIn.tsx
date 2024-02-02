32aa10d8fee0c383824c1f9977e5eb7d569ca555
Leonardo <leo-feitosa@live.com>
2024-02-02 15:27:09 -0300

    feat: add title in singIn

--- src/screens/singIn.tsx
+++ src/screens/singIn.tsx
@@ -1,5 +1,6 @@
 import BackgroundImg from '@assets/background.png';
-import { VStack, Image } from 'native-base';
+import { VStack, Image, Text, Center } from 'native-base';
+import LogoSvg from '@assets/logo.svg';
 
 export function SignIn() {
   return (
@@ -8,7 +9,16 @@ export function SignIn() {
         source={BackgroundImg}
         alt="Pessoas treinando"
         resizeMode="contain"
+        position="absolute"
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize={'sm'}>
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
     </VStack>
   );
 }
