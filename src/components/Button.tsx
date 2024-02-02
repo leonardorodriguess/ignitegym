2aff0bcc525a3c75eacdfe66f90ec0e2a1e5d933
Leonardo <leonardorodrigues@grendene.com.br>
2024-02-02 16:29:31 -0300

    feat: style button outline

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,19 +4,25 @@ type Props = IButtonProps & {
   title: string;
 };
 
-export function Button({ title, ...rest }: Props) {
+export function Button({ title, variant, ...rest }: Props) {
   return (
     <ButtonNativeBase
       w={'full'}
       h={14}
-      bg={'green.700'}
+      bg={variant === 'outline' ? 'transparent' : 'green.700'}
+      borderWidth={variant === 'outline' ? 1 : 0}
+      borderColor={'green.500'}
       rounded={'sm'}
       _pressed={{
-        bg: 'green.500',
+        bg: variant === 'outline' ? 'gray.500' : 'green.500',
       }}
       {...rest}
     >
-      <Text color="white" fontSize={'sm'} fontFamily={'heading'}>
+      <Text
+        color={variant === 'outline' ? 'green.500' : 'white'}
+        fontSize={'sm'}
+        fontFamily={'heading'}
+      >
         {title}
       </Text>
     </ButtonNativeBase>
