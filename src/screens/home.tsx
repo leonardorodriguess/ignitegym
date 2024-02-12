96b229fe25e77ffdad1652ee254b1348214ba7f3
leonardo <leo-feitosa@live.com>
2024-02-12 15:53:30 -0300

    style: ajusts text on screens

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -2,8 +2,8 @@ import { Center, Text } from "native-base";
 
 export function Home () {
   return (
-    <Center>
-      <Text color={"red.100"}> Home </Text>
+    <Center flex={1}>
+      <Text color={"white"}> Home </Text>
     </Center>
   )
 }
