d5481d8197a45b0ae34c7d07a64c15d78b2504a2
leonardo <leo-feitosa@live.com>
2024-02-12 16:57:29 -0300

    style: home header

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,11 +2,11 @@ import { HStack, Heading, Text, VStack } from 'native-base';
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bg={"gray.400"} pt={16} pb={5} px={8} alignItems={"center"}>
       <VStack>
-        <Text color="gray.100">Olá,</Text>
+        <Text color="gray.100" fontSize={"md"}>Olá,</Text>
 
-        <Heading color="gray.100">Leonardo</Heading>
+        <Heading color="gray.100" fontSize={"md"} >Leonardo</Heading>
       </VStack>
     </HStack>
   );
