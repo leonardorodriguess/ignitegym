64c14edbca05683c44b750182d70c5ec3d625033
leonardo <leo-feitosa@live.com>
2024-02-12 19:32:23 -0300

    feat: add image user on profile

--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -1,9 +1,24 @@
-import { Center, Text } from "native-base";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, ScrollView, VStack } from "native-base";
 
 export function Profile () {
   return (
-    <Center flex={1}>
-      <Text color={"white"}>Profile </Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto 
+            source={{uri: 'http://github.com/leonardorodriguess.png'}}
+            alt="Imagem do usuário"
+            size={33}
+          />
+
+        </Center>
+      </ScrollView>
+
+
+    </VStack>
   )
 }
