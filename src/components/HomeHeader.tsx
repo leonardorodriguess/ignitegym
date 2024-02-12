82d84232b717158c5b4f0ca96820a17c6441c477
leonardo <leo-feitosa@live.com>
2024-02-12 17:18:29 -0300

    feat: finish home header

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -14,7 +14,7 @@ export function HomeHeader() {
         source={{ uri: 'https://github.com/leonardoRodriguess.png' }}
       />
 
-      <VStack>
+      <VStack flex={1}>
         <Text color="gray.100" fontSize={'md'}>
           Olá,
         </Text>
