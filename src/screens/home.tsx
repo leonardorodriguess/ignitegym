29c10356d6d30d026e499bf4a9bf5f7943f63149
leonardo <leo-feitosa@live.com>
2024-02-12 17:44:39 -0300

    style: selected group

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,14 +1,21 @@
-import { VStack } from "native-base";
+import { useState } from "react";
+import { HStack, VStack } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
 
 export function Home () {
+  const [groupSelected, setGroupSelected] = useState('costas');
+  
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group name="costas" />
+      <HStack>
+        <Group name="costas" isActive={groupSelected === "costas"} onPress={() => setGroupSelected("costas")}/>
+        <Group name="ombro" isActive={groupSelected === "ombro"} onPress={() => setGroupSelected("ombro")}/>
+
+      </HStack>
     </VStack>
   )
 }
