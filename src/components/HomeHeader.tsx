7a2e0f92b9da8127ededdf28a8e1bc5023d5e132
leonardo <leo-feitosa@live.com>
2024-02-12 17:10:29 -0300

    feat: add user photo

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,10 @@
 import { HStack, Heading, Text, VStack } from 'native-base';
+import { UserPhoto } from './UserPhoto';
 
 export function HomeHeader() {
   return (
     <HStack bg={"gray.400"} pt={16} pb={5} px={8} alignItems={"center"}>
+      <UserPhoto source={{ uri: 'https://github.com/leonardoRodriguess.png'}} alt="Imagem do usuário" size={16} mr={4}/>
       <VStack>
         <Text color="gray.100" fontSize={"md"}>Olá,</Text>
 
