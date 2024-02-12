8eff66cb855fc5d2328a09e48db5e54ed5160d6d
leonardo <leo-feitosa@live.com>
2024-02-12 16:09:10 -0300

    feat: remove label on bottom tabs

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,16 +1,19 @@
-import { createBottomTabNavigator, BottomTabNavigationProp  } from "@react-navigation/bottom-tabs"
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationProp,
+} from '@react-navigation/bottom-tabs';
 
-import { Home } from "@screens/home";
-import { Profile } from "@screens/profile";
-import { History } from "@screens/history";
-import { Exercise } from "@screens/exercise";
+import { Home } from '@screens/home';
+import { Profile } from '@screens/profile';
+import { History } from '@screens/history';
+import { Exercise } from '@screens/exercise';
 
-type AppRoutes = { 
+type AppRoutes = {
   home: undefined;
   exercise: undefined;
   profile: undefined;
   history: undefined;
-}
+};
 
 export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 
@@ -18,11 +21,11 @@ const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
   return (
-    <Navigator screenOptions={{headerShown: false}}>
+    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
       <Screen name="home" component={Home} />
       <Screen name="history" component={History} />
       <Screen name="profile" component={Profile} />
       <Screen name="exercise" component={Exercise} />
     </Navigator>
-  )
+  );
 }
