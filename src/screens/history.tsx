8ffb2a339c80ac616971e61de60c5d80be159e7a
leonardo <leo-feitosa@live.com>
2024-02-12 19:27:14 -0300

    feat: add empty list on history

--- src/screens/history.tsx
+++ src/screens/history.tsx
@@ -1,33 +1,41 @@
-import { HistoryCard } from "@components/HistoryCard";
-import { ScreenHeader } from "@components/ScreenHeader";
-import { VStack, SectionList, Heading } from "native-base";
-import { useState } from "react";
+import { HistoryCard } from '@components/HistoryCard';
+import { ScreenHeader } from '@components/ScreenHeader';
+import { VStack, SectionList, Heading, Text } from 'native-base';
+import { useState } from 'react';
 
-export function History () {
+export function History() {
   const [exercises, setExercises] = useState([
     {
       title: '26.08.22',
-      data: ["Puxada frontal", "Remada unilateral"]
+      data: ['Puxada frontal', 'Remada unilateral'],
     },
     {
       title: '27.08.22',
-      data: ["Puxada frontal"]
-    }
+      data: ['Puxada frontal'],
+    },
   ]);
   return (
     <VStack flex={1}>
-      <ScreenHeader title={"Histórico de Exercícios"}/>
+      <ScreenHeader title={'Histórico de Exercícios'} />
 
       <SectionList
-        sections={exercises}
-        keyExtractor={item => item}
-        renderItem={({item}) => ( <HistoryCard />)}
-
-        renderSectionHeader={({section}) => (
-          <Heading color="gray.200" fontSize={"md"} mt="10" mb={3}>{section.title}</Heading>
-        )}
         px={8}
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize={'md'} mt="10" mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        contentContainerStyle={exercises.length === 0 && {flex: 1, justifyContent: 'center'}}
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign={"center"}> 
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercícios hoje ?
+          </Text>
+        )}
       />
     </VStack>
-  )
+  );
 }
