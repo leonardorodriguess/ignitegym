b76c6a2f228f92d0c5994326c0c81a36351b4863
leonardo <leo-feitosa@live.com>
2024-02-13 11:39:04 -0300

    feat: add image exerciseon screen exercise

--- src/screens/exercise.tsx
+++ src/screens/exercise.tsx
@@ -1,11 +1,11 @@
-import { HStack, Heading, Icon, Text, VStack } from 'native-base';
+import { HStack, Heading, Icon, Image, Text, VStack } from 'native-base';
 import { Feather } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes';
 
-import BodySvg from "@assets/body.svg";
+import BodySvg from '@assets/body.svg';
 
 export function Exercise() {
   const navigation = useNavigation<AppNavigatorRoutesProps>();
@@ -28,16 +28,31 @@ export function Exercise() {
           justifyContent={'space-between'}
         >
           <Heading color="gray.100" fontSize="lg" flexShrink={1}>
-            Puxador frontal  
+            Puxador frontal
           </Heading>
 
-          <HStack alignItems={"center"}>
+          <HStack alignItems={'center'}>
             <BodySvg />
             <Text color="gray.200" ml={1} textTransform={'capitalize'}>
               Costas
             </Text>
           </HStack>
         </HStack>
+      </VStack>
+
+      <VStack p={8}>
+        <Image
+          mb={3}
+          w="full"
+          h={80}
+          rounded={"lg"}
+          overflow={"hidden"}
+          resizeMode={'cover'}
+          source={{
+            uri: 'https://www.treinus.com.br/blog/wp-content/uploads/2020/05/tipos-de-exercicios-f%C3%ADsicos.jpg',
+          }}
+          alt="Nome do exercício"
+        />
       </VStack>
     </VStack>
   );
