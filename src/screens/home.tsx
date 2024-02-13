73291055855ee9713d3c5b126bdbb0243980ccaa
leonardo <leo-feitosa@live.com>
2024-02-13 10:53:00 -0300

    feat: add route to exercise

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { FlatList, HStack, Heading, Text, VStack } from 'native-base';
 
-import { HomeHeader } from '@components/HomeHeader';
 import { Group } from '@components/Group';
+import { HomeHeader } from '@components/HomeHeader';
 import { ExerciseCard } from '@components/ExerciseCard';
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
 
 export function Home() {
   const [groups, setGroups] = useState([
@@ -15,6 +17,12 @@ export function Home() {
 
   const [exercises, setExercises] = useState(['Puxada frontal', 'Remanda curvada', 'remada unilateral', 'levantamento terra'])
   const [groupSelected, setGroupSelected] = useState('Costas');
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>(); 
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise');
+  }
 
   return (
     <VStack flex={1}>
@@ -51,7 +59,7 @@ export function Home() {
         <FlatList 
           data={exercises}
           keyExtractor={(item) => item}
-          renderItem={({item}) => <ExerciseCard/>}
+          renderItem={({item}) => <ExerciseCard onPress={handleOpenExerciseDetails}/>}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{ paddingBottom: 20 }}
         />
