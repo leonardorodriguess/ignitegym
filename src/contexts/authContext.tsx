33429d850293a717e8404153e3224723f6813804
Leonardo <leo-feitosa@live.com>
2024-03-03 16:41:25 -0300

    feat: use state in context auth

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -1,27 +1,31 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useState } from 'react';
 
-import { UserDTO  } from "@dtos/userDTO";
+import { UserDTO } from '@dtos/userDTO';
 
 export type AuthContextDataProps = {
   user: UserDTO;
-}
+};
 
-export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+export const AuthContext = createContext<AuthContextDataProps>(
+  {} as AuthContextDataProps,
+);
 
 type AuthContextProviderProps = {
   children: ReactNode;
-}
+};
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState({
+    id: '1',
+    name: 'Leonardo',
+    email: 'leo-feitosa@live.com',
+    avatar: 'string',
+  });
+
   return (
     <AuthContext.Provider
       value={{
-        user: {
-          id: '1',
-          name: 'Leonardo',
-          email: 'leo-feitosa@live.com',
-          avatar: 'string',
-        },
+        user,
       }}
     >
       {children}
